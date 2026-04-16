37a69e1412a86f15bb9916f0106d3012f851b218
charanepuri <charanepuri26@gmail.com>
2026-04-16 23:33:39 +0530

    Second Commit

--- jango_project_deployee/settings.py
+++ jango_project_deployee/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'jango_project_deployee.wsgi.application'
 #     }
 # }
 
-import os
+
 import os 
 if os.environ.get("RENDER"): 
     # Production (Render + Aiven) 
@@ -100,7 +100,7 @@ else:
     DATABASES = { 
         "default": { 
             "ENGINE": "django.db.backends.mysql", 
-            "NAME": "djnago_project_deployee", 
+            "NAME": "djnago_project_deployee",
             "USER": "root", 
             "PASSWORD": "charan@261103", 
             "HOST": "localhost", 
